5037756b13445cfd87437c0cb7278124eb0c0153
Eugene Mickhnitskyy <todesstoss@gmail.com>
2019-05-28 16:00:15 +0300

    babel plugin and config added

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,14 @@
 import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
 
 export default [
   // browser-friendly UMD build
   {
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
     input: 'src/index.js',
     external: ['react'],
     output: {
@@ -13,6 +19,11 @@ export default [
   },
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
     input: 'src/index.js',
     external: ['react'],
     output: [
