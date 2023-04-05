f1f332e8b8b5d84bb802c4dca32618f49776d93f
Yevgenii Mikhnytskyi <yevgenii.mikhnytskyi@fiverr.com>
2023-04-05 12:04:12 +0300

    remove terser

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
-## 2.0.1
+## 2.0.2
+
+### Fix
+
+- Terser plugin removed
 
 ### Fix
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const production = process.env.BUILD === 'production';
 
 function getConfig({ dir, format }) {
   return {
@@ -16,7 +13,6 @@ function getConfig({ dir, format }) {
     plugins: [
       peerDepsExternal(),
       typescript({ outDir: dir }),
-      production && terser(),
     ],
   }
 }
